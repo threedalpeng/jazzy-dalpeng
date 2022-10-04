695ed59c1c22e09de613d47caaeea91460f4390b
Kim Jeong-won <threedalpeng@gmail.com>
2022-10-04 23:19:34 +0900

    Create Clip for Canvas

---
+++ src/components/canvas/Clip.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { onCanvasRender } from "./context";
+
+  export let x: number;
+  export let y: number;
+  export let width: number;
+  export let height: number;
+
+  onCanvasRender(({ context2d: ctx }) => {
+    ctx.clearRect(x, y, width, height);
+  });
+</script>
