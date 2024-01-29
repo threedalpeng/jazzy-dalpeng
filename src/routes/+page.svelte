c18c51665a07f6c7490eefb5128c581cbdb37f5f
Kim Jeong-won <threedalpeng@gmail.com>
2024-01-29 16:15:17 +0900

    feat: add a link to sample in the index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <p>
 	Now JazzyDalpeng is temporarily closed, and on being developed hard.
 	<br /> I hope it'll open soon :)
 </p>
+<a
+	class="cursor-pointer underline text-indigo hover:text-indigo-600"
+	href="{base}/practice/render-sample">Here's Sample</a
+>
