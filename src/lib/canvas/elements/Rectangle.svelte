29c4cd86afacd43050e85bafd5ec3bc53807d77f
Kim Jeong-won <threedalpeng@gmail.com>
2024-02-27 04:38:43 +0900

    feat: add rounded property to Canvas Rectangle

--- src/lib/canvas/elements/Rectangle.svelte
+++ src/lib/canvas/elements/Rectangle.svelte
@@ -10,6 +10,7 @@
 		fillStyle?: CanvasStyle;
 		strokeStyle?: CanvasStyle;
 		lineWidth?: number;
+		rounded?: number;
 	}
 
 	const {
@@ -21,13 +22,24 @@
 		strokeStyle = '#000',
 		lineWidth = 1,
 		active = false,
+		rounded = 0,
 		...rest
 	}: RectangleProps & ForwardHitRegionProps = $props();
 
 	const render = (ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D) => {
+		const xr = rounded < width / 2 ? rounded : width / 2;
+		const yr = rounded < height / 2 ? rounded : height / 2;
+
 		ctx.lineWidth = lineWidth;
 		ctx.beginPath();
-		ctx.rect(x, y, width, height);
+		ctx.ellipse(x + xr, y + yr, xr, yr, 0, Math.PI, Math.PI * 1.5);
+		ctx.lineTo(x + width - xr, y);
+		ctx.ellipse(x + width - xr, y + yr, xr, yr, 0, Math.PI * 1.5, Math.PI * 2);
+		ctx.lineTo(x + width, y + height - yr);
+		ctx.ellipse(x + width - xr, y + height - yr, xr, yr, 0, 0, Math.PI * 0.5);
+		ctx.lineTo(x + xr, y + height);
+		ctx.ellipse(x + xr, y + height - yr, xr, yr, 0, Math.PI * 0.5, Math.PI * 1);
+		ctx.lineTo(x, y + yr);
 		ctx.fill();
 		ctx.stroke();
 	};
