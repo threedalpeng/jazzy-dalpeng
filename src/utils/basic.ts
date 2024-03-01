9e52fc1013dc17b0284d94e5fa9ec1da08e197e7
Kim Jeong-won <threedalpeng@gmail.com>
2024-03-01 15:32:13 +0900

    feat: range utils

--- src/utils/basic.ts
+++ src/utils/basic.ts
@@ -3,3 +3,7 @@ export function debounce(callback: TimerHandler, timeout = 1000) {
 	clearTimeout(debounceTimer);
 	debounceTimer = window.setTimeout(callback, timeout);
 }
+
+export function range(start: number, end: number) {
+	return [...Array(end - start).keys()].map((i) => i + start);
+}
